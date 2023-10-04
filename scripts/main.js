283aebbebc1cb8dfb0c333620a7cae4b96b134ba
Siddharth-Kamlay <siddharthkamlay777@gmail.com>
2023-10-04 23:50:55 +0530

    commit

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-logo.png") {
+    myImage.setAttribute("src", "images/firefox-logo-2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-logo.png");
+  }
+};
+
+let myButton=document.querySelector("button");
+myButton.onclick = () => {
+  setUserName();
+};
+
+
+let myHeading=document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Welcome to Mozilla, ${myName}`;
+  }
+}
+
+
+if(!localStorage.getItem("name")){
+  setUserName();
+}
+else{
+  const storedName=localStorage.getItem("name");
+  myHeading.textContent(`Welcome to Mozilla, ${storedName}`);
+}
+
